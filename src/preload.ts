6850986b68398fa7db91762fe986fd9061c97b06
Jon Campbell <joncamp@outlook.com>
2024-12-26 12:01:05 -0800

    Fails to run.
    ```
          WARNING in ./node_modules/@azure/monitor-opentelemetry/dist-esm/src/traces/azureFnHook.js 9:40-72
          Module not found: Error: Can't resolve '@azure/functions-core' in
          'D:\opentelemetry-webpack-typescript\node_modules\@azure\monitor-opentelemetry\dist-esm\src\traces'
          @ ./node_modules/@azure/monitor-opentelemetry/dist-esm/src/traces/handler.js 14:0-51 39:39-57
          @ ./node_modules/@azure/monitor-opentelemetry/dist-esm/src/index.js 8:0-48 52:29-41
          @ ./src/preload.ts 5:32-71
    
          WARNING in ./node_modules/@grpc/grpc-js/build/src/channelz.js
          Invalid dependencies have been reported by plugins or loaders for this module. All reported dependencies
          need to be absolute paths.
          Invalid dependencies may lead to broken watching and caching.
          As best effort we try to convert all invalid values to absolute paths and converting globs into context
          dependencies, but this is deprecated behavior.
          Loaders: Pass absolute paths to this.addDependency (existing files), this.addMissingDependency (not existing
          files), and this.addContextDependency (directories).
          Plugins: Pass absolute paths to fileDependencies (existing files), missingDependencies (not existing files),
          and contextDependencies (directories).
          Globs: They are not supported. Pass absolute path to the directory as context dependencies.
          The following invalid values have been reported:
          * "D:/opentelemetry-webpack-typescript/node_modules/@grpc/grpc-js/proto/channelz.proto"
          @ ./node_modules/@grpc/grpc-js/build/src/index.js 117:17-38 134:17-38
          @ ./node_modules/@azure/monitor-opentelemetry/node_modules/@opentelemetry/otlp-grpc-exporter-base/build/src/
          grpc-exporter-transport.js 29:9-33 37:9-33 45:9-33
          @ ./node_modules/@azure/monitor-opentelemetry/node_modules/@opentelemetry/otlp-grpc-exporter-base/build/src/
          OTLPGRPCExporterNodeBase.js 22:34-70
          @ ./node_modules/@azure/monitor-opentelemetry/node_modules/@opentelemetry/otlp-grpc-exporter-base/build/src/
          index.js 19:33-70
          @ ./node_modules/@azure/monitor-opentelemetry/node_modules/@opentelemetry/exporter-logs-otlp-grpc/build/src/
          OTLPLogExporter.js 20:34-83
          @ ./node_modules/@azure/monitor-opentelemetry/node_modules/@opentelemetry/exporter-logs-otlp-grpc/build/src/
          index.js 31:13-41
          @ ./node_modules/@azure/monitor-opentelemetry/node_modules/@opentelemetry/sdk-node/build/src/sdk.js 25:34-83
          @ ./node_modules/@azure/monitor-opentelemetry/node_modules/@opentelemetry/sdk-node/build/src/index.js
          30:12-28
          @ ./node_modules/@azure/monitor-opentelemetry/dist-esm/src/index.js 5:0-50 81:14-21
          @ ./src/preload.ts 5:32-71
    ```

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,12 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { useAzureMonitor, AzureMonitorOpenTelemetryOptions } from "@azure/monitor-opentelemetry";
+
+const options: AzureMonitorOpenTelemetryOptions = {
+  azureMonitorExporterOptions: {
+    connectionString:
+      process.env.APPLICATION_INSIGHTS_CONNECTION_STRING,
+  },
+};
+useAzureMonitor(options);
